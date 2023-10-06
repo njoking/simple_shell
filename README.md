d927875e879bea3a89c4a3bde293ae32b1a6f638
kenyanroot <ndedaanaco@gmail.com>
2023-10-06 02:06:16 -0700

    beautifull betty

--- README.md
+++ README.md
@@ -1,7 +1,3 @@
-
-We're moving to Discord!
-In a few days, we will be leaving Slack in favor of Discord 🎉
-Click here for more information
 0x16. C - Simple Shell
 C
 Group project
---
+++ hello_betty.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point of the program
+ *
+ * Return: 0 on success
+ */
+int main(void)
+{
+    printf("Hello, Betty!\n");
+    return (0);
+}
