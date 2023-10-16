d8953f1768510a4c4640ac62d70b529620552e21
njoking <njokingugi9462@gmail.com>
2023-10-16 14:51:17 -0700

    task 4n 5

--- shell.h
+++ shell.h
@@ -1,7 +1,13 @@
-#ifndef SHELL_H
-#define SHELL_H
 #include <stdio.h>
-#include <string.h>
 #include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
 
-#endif
+extern char **environ;
+
+#define MAX_BUFFER_SIZE 1024
+
+/* ... rest of your code ... */
+
