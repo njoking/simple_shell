b97db388b77bda7303ce432a925d34ccff9cde55
kenyanroot <ndedaanaco@gmail.com>
2023-10-17 12:57:15 -0700

    yeeey

--- shell.c
+++ shell.c
@@ -1,98 +1,68 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 #include <unistd.h>
-#include <sys/types.h>
 #include <sys/wait.h>
+#include <string.h>
 
-extern char **environ;
-
-#define MAX_BUFFER_SIZE 1024
-
-/**
- * execute_command - function to execute a command
- * @command: command to be executed
- */
-void execute_command(char *command)
+int main(int argc, char **argv)
 {
-	char *args[MAX_BUFFER_SIZE];
-	char *token;
-	int i = 0;
+    /* Declare all variables at the beginning */
+    char *line = NULL;
+    size_t len = 0;
+    ssize_t read;
+    pid_t child_pid;
+    int status;
+    int is_interactive;
 
-	token = strtok(command, " \t\n");
-	while (token != NULL)
-	{
-		args[i] = token;
-		token = strtok(NULL, " \t\n");
-		i++;
-	}
-	args[i] = NULL;
+    /* Suppress unused warning */
+    (void)argc;  /* suppress unused warning */
 
-	char *path = getenv("PATH");
-	char *path_copy = strdup(path);
-	char *path_token = strtok(path_copy, ":");
+    /* Check if it's interactive mode */
+    is_interactive = isatty(STDIN_FILENO);
 
-	while (path_token != NULL)
-	{
-		char command_path[MAX_BUFFER_SIZE];
+    while (1)
+    {
+        if (is_interactive)
+            printf("($) ");
 
-		snprintf(command_path, sizeof(command_path), "%s/%s", path_token, args[0]);
-		if (access(command_path, X_OK) == 0)
-		{
-			pid_t pid = fork();
+        read = getline(&line, &len, stdin);
 
-			if (pid == 0)
-			{
-				if (execv(command_path, args) == -1)
-				{
-					perror("Execv failed");
-					exit(EXIT_FAILURE);
-				}
-			}
-			else if (pid > 0)
-			{
-				waitpid(pid, NULL, 0);
-				break;
-			}
-			else
-			{
-				perror("Fork failed");
-			}
-		}
-		path_token = strtok(NULL, ":");
-	}
-	free(path_copy);
+        if (read == -1)  /* Handling EOF (Ctrl+D) */
+        {
+            if (is_interactive)
+                printf("\n");
+            break;
+        }
+
+        /* Remove the trailing newline character */
+        if (line[read - 1] == '\n')
+            line[read - 1] = '\0';
+
+        child_pid = fork();
+
+        if (child_pid == 0)  /* Child process */
+        {
+            char *exec_args[2];
+            exec_args[0] = line;
+            exec_args[1] = NULL;
+
+            if (execve(line, exec_args, NULL) == -1)
+            {
+                fprintf(stderr, "%s: 1: %s: not found\n", argv[0], line);
+                exit(127);
+            }
+        }
+        else if (child_pid < 0)
+        {
+            perror("Error forking");
+        }
+        else  /* Parent process */
+        {
+            wait(&status);
+        }
+    }
+
+    free(line);
+
+    return 0;
 }
-
-/**
- * main - main function for the shell
- * Return: 0 on success
- */
-int main(void)
-{
-	char buffer[MAX_BUFFER_SIZE];
-
-	while (1)
-	{
-		printf("$ ");
-		fflush(stdout);
-		if (fgets(buffer, sizeof(buffer), stdin) == NULL)
-			break;
-		size_t length = strlen(buffer);
-		if (buffer[length - 1] == '\n')
-			buffer[length - 1] = '\0';
-
-		if (strcmp(buffer, "exit") == 0)
-			break;
-		if (strcmp(buffer, "env") == 0)
-		{
-			for (int i = 0; environ[i]; i++)
-				printf("%s\n", environ[i]);
-		}
-		else
-			execute_command(buffer);
-	}
-	printf("Exiting shell...\n");
-	return (0);
-}
-
