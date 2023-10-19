1c90adaa2e57430fd8cc26ffe6558fd3d46ad790
kenyanroot <ndedaanaco@gmail.com>
2023-10-19 14:12:24 -0700

    yeyj

---
+++ shell.c
@@ -0,0 +1,22 @@
+#include "shell.h"
+/*
+ *
+ */
+
+int main(void) {
+    char command[120];
+
+    while (true) {
+        display_prompt();
+        read_command(command, sizeof(command));
+        execute_command(command);
+    }
+
+
+
+
+
+    
+
+    return 0;
+}
