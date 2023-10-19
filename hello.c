912b9154d89d2f8dc4f7118b240c3f8a8bcbf365
kenyanroot <ndedaanaco@gmail.com>
2023-10-19 05:06:47 -0700

    Purge

---
+++ hello.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point for the program
+ *
+ * Return: 0 if successful, otherwise return a non-zero value
+ */
+int main(void)
+{
+    printf("Hello, Betty!\n");
+    return (0);
+}
