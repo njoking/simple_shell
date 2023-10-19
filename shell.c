7b8e291ba4225f837ec6844a01d766d69abfa099
kenyanroot <ndedaanaco@gmail.com>
2023-10-19 08:41:20 -0700

    k

--- shell.c
+++ shell.c
@@ -5,33 +5,47 @@
 #include <sys/types.h>
 #include <sys/wait.h>
 
-int main() {
-    char *line = NULL;
-    size_t len = 0;
-    ssize_t nread;
+/**
+ * main - Entry point for the simple shell
+ * 
+ * Return: 0 on success, EXIT_FAILURE on error
+ */
+int main(void)
+{
+	char *line = NULL;
+	size_t len = 0;
+	ssize_t nread;
+	pid_t pid;
 
-    while (1) {
-        write(STDOUT_FILENO, "#cisfun$ ", 9);
-        nread = getline(&line, &len, stdin);
-        
-        if (nread == -1 && feof(stdin)) {
-            exit(EXIT_SUCCESS);
-        }
-        
-        line[nread - 1] = '\0'; // Remove the newline character
-        
-        pid_t pid = fork();
-        if (pid == 0) {
-            char *argv[] = {line, NULL};
-            execve(line, argv, NULL);
-            perror("execve");
-            exit(EXIT_FAILURE);
-        } else if (pid > 0) {
-            wait(NULL);
-        } else {
-            perror("fork");
-        }
-    }
-    free(line);
-    return 0;
+	while (1)
+	{
+		write(STDOUT_FILENO, "#cisfun$ ", 9);
+		nread = getline(&line, &len, stdin);
+
+		if (nread == -1 && feof(stdin))
+		{
+			exit(EXIT_SUCCESS);
+		}
+
+		line[nread - 1] = '\0';
+		pid = fork();
+
+		if (pid == 0)
+		{
+			char *argv[] = {line, NULL};
+			execve(line, argv, NULL);
+			perror("execve");
+			exit(EXIT_FAILURE);
+		}
+		else if (pid > 0)
+		{
+			wait(NULL);
+		}
+		else
+		{
+			perror("fork");
+		}
+	}
+	free(line);
+	return (0);
 }
